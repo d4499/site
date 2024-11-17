2f289e5f47e55283acf4aec461775090973c82a4
dalton <dakribe94@gmail.com>
2024-11-17 14:56:24 -0600

    configure shiki

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from "@solidjs/start/config";
+import rehypeShiki from "@shikijs/rehype";
 /* @ts-ignore */
 import pkg from "@vinxi/plugin-mdx";
 
 const { default: mdx } = pkg;
 export default defineConfig({
-  extensions: ["mdx", "md"],
-  vite: {
-    plugins: [
-      mdx.withImports({})({
-        jsx: true,
-        jsxImportSource: "solid-js",
-        providerImportSource: "solid-mdx"
-      })
-    ]
-  }
+	extensions: ["mdx", "md"],
+	vite: {
+		plugins: [
+			mdx.withImports({})({
+				jsx: true,
+				jsxImportSource: "solid-js",
+				providerImportSource: "solid-mdx",
+				remarkPlugins: [],
+				rehypePlugins: [
+					[
+						rehypeShiki,
+						{
+							theme: "catppuccin-mocha",
+						},
+					],
+				],
+			}),
+		],
+	},
 });
