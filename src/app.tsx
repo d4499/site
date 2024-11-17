36bc874770d4d638ef4790ac4bf3e1c8790184f8
dalton <dakribe94@gmail.com>
2024-11-17 15:34:00 -0600

    navbar

--- src/app.tsx
+++ src/app.tsx
@@ -2,19 +2,19 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { Navbar } from "./components/nav";
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <main>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </main>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={(props) => (
+				<main>
+					<Navbar />
+					<Suspense>{props.children}</Suspense>
+				</main>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
